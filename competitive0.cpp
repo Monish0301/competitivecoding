4bc47d8db071edbab00eaf627ecae1defa4bf6f1
Monish <monishgovind0301@gmail.com>
2021-08-08 09:38:57 +0530

    add

--- competitive0.cpp
+++ competitive0.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
-	cout<<"Welcome to my Competitive coding programs";
+	cout<<"Welcome to my c++ coding programs";
+	cout<<"verified";
+	cout<<"pc also verified";
 	return 0;
 }
